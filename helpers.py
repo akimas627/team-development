5a1441be6aba84f1e2f5134d24332df191ef1296
Ry268 <ryotaro545@gmail.com>
2022-09-15 21:58:35 +0900

    emailが有効かどうかをチェックする関数の実装

---
+++ helpers.py
@@ -0,0 +1,7 @@
+import re
+def check_email(email: str):
+    pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
